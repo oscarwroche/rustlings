d62de32d2231524c1d68edf03a763a687aa0e51d
Oscar Roche <oscarroche@oscarwroche.local>
2023-05-11 01:13:39 +0200

    :construction: threads and iterators

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,12 +1,47 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+struct Counter {
+    count: usize,
+    next_factorial: usize,
+}
+
+// we want our count to start at one, so let's add a new() method to help.
+// This isn't strictly necessary, but is convenient. Note that we start
+// `count` at zero, we'll see why in `next()`'s implementation below.
+impl Counter {
+    fn new() -> Counter {
+        Counter {
+            count: 1,
+            next_factorial: 1,
+        }
+    }
+}
+
+// Then, we implement `Iterator` for our `Counter`:
+
+impl Iterator for Counter {
+    // we will be counting with usize
+    type Item = usize;
+
+    // next() is the only required method
+    fn next(&mut self) -> Option<Self::Item> {
+        // Increment our count. This is why we started at zero.
+        let factorial = self.next_factorial;
+        self.next_factorial *= self.count;
+        self.count += 1;
+
+        // Check to see if we've finished counting or not.
+        Some(factorial)
+    }
+}
 
 pub fn factorial(num: u64) -> u64 {
+    let mut counter = Counter::new();
+    counter.nth(num as usize).unwrap() as u64
     // Complete this function to return the factorial of num
     // Do not use:
-    // - return
+    // - retunr
     // Try not to use:
     // - imperative style loops (for, while)
     // - additional variables
